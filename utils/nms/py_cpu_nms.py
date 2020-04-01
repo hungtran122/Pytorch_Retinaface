6e9cef7c498012d1aabdd2a5f58a23fa599ba068
hungficha <hung.tm@ficha.jp>
2020-04-01 12:34:48 +0900

    updated

--- utils/nms/py_cpu_nms.py
+++ utils/nms/py_cpu_nms.py
@@ -8,31 +8,64 @@
 import numpy as np
 
 def py_cpu_nms(dets, thresh):
-    """Pure Python NMS baseline."""
-    x1 = dets[:, 0]
-    y1 = dets[:, 1]
-    x2 = dets[:, 2]
-    y2 = dets[:, 3]
-    scores = dets[:, 4]
+	"""Pure Python NMS baseline."""
+	x1 = dets[:, 0]
+	y1 = dets[:, 1]
+	x2 = dets[:, 2]
+	y2 = dets[:, 3]
+	scores = dets[:, 4]
 
-    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
-    order = scores.argsort()[::-1]
+	areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+	order = scores.argsort()[::-1]
 
-    keep = []
-    while order.size > 0:
-        i = order[0]
-        keep.append(i)
-        xx1 = np.maximum(x1[i], x1[order[1:]])
-        yy1 = np.maximum(y1[i], y1[order[1:]])
-        xx2 = np.minimum(x2[i], x2[order[1:]])
-        yy2 = np.minimum(y2[i], y2[order[1:]])
+	keep = []
+	while order.size > 0:
+		i = order[0]
+		keep.append(i)
+		xx1 = np.maximum(x1[i], x1[order[1:]])
+		yy1 = np.maximum(y1[i], y1[order[1:]])
+		xx2 = np.minimum(x2[i], x2[order[1:]])
+		yy2 = np.minimum(y2[i], y2[order[1:]])
 
-        w = np.maximum(0.0, xx2 - xx1 + 1)
-        h = np.maximum(0.0, yy2 - yy1 + 1)
-        inter = w * h
-        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+		w = np.maximum(0.0, xx2 - xx1 + 1)
+		h = np.maximum(0.0, yy2 - yy1 + 1)
+		inter = w * h
+		ovr = inter / (areas[i] + areas[order[1:]] - inter)
 
-        inds = np.where(ovr <= thresh)[0]
-        order = order[inds + 1]
+		inds = np.where(ovr <= thresh)[0]
+		order = order[inds + 1]
 
-    return keep
+	return keep
+
+def customized_cpu_nms(dets, thresh, total_models=3):
+	"""Pure Python NMS baseline."""
+	x1 = dets[:, 0]
+	y1 = dets[:, 1]
+	x2 = dets[:, 2]
+	y2 = dets[:, 3]
+	scores = dets[:, 5]
+	areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+	order = scores.argsort()[::-1]
+	keep = []
+	ignores = []
+	while order.size > 0:
+		i = order[0]
+		xx1 = np.maximum(x1[i], x1[order[1:]])
+		yy1 = np.maximum(y1[i], y1[order[1:]])
+		xx2 = np.minimum(x2[i], x2[order[1:]])
+		yy2 = np.minimum(y2[i], y2[order[1:]])
+
+		w = np.maximum(0.0, xx2 - xx1 + 1)
+		h = np.maximum(0.0, yy2 - yy1 + 1)
+		inter = w * h
+		ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+		inds = np.where(ovr <= thresh)[0]
+		neg_inds = np.where(ovr > thresh)[0]
+		num_models = len(set(dets[order[neg_inds+1], 4].tolist() + [dets[i, 4]]))
+		if num_models < total_models:
+			ignores.append(i)
+		else:
+			keep.append (i)
+		order = order[inds+1]
+	return keep, ignores
